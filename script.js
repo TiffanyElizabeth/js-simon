c0e336736a47c2dc361dee31f6041b2f19d8bee8
tiffanybrezak <tiffany.elizabeth.b@gmail.com>
2024-12-17 22:41:36 +0100

    steps breakdown js

--- script.js
+++ script.js
@@ -8,3 +8,11 @@ Pensate prima in italiano.
 Dividete in piccoli problemi la consegna.
 Individuate gli elementi di cui avete bisogno per realizzare il programma.
 Immaginate la logica come fosse uno snack: "Dati 2 array di numeri, indica quali e quanti numeri ci sono in comune tra i due array */
+
+// 1. stampa 5 numeri casuali (max 2 digits each)
+
+// 2. parte un timer di 30 secondi
+
+// 3. quando finisce il timer, i numeri scompaiono e appaiono invece 5 input in cui l'utente deve inserire i numeri che ha visto precedentemente 
+
+// 4. dopo l'utente clicca "conferma", il software dice quanti e quali dei numeri da indovinare sono stati individuati 
